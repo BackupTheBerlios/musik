2807970d1c110804a62a0c66331d862d4cfdbb66
avatar5d <avatar5d>
2003-08-01 05:15:57 +0000

    Cleanup

--- Musik/MusikApp.cpp
+++ Musik/MusikApp.cpp
@@ -9,7 +9,8 @@
  *  See the file "license.txt" for information on usage and redistribution
  *  of this file, and for a DISCLAIMER OF ALL WARRANTIES.
 */
-// For compilers that support precompilation, includes "wx/wx.h".
+
+//--- For compilers that support precompilation, includes "wx/wx.h". ---//
 #include "wx/wxprec.h"
 #include "MusikApp.h"
 IMPLEMENT_APP( MusikApp )
