aa05fe3854d567dd2bb5d1e0d740f5120424363a
avatar5d <avatar5d>
2003-10-28 19:43:31 +0000

    CMusikSong::GetField, CMusikPlaylist::GetSong implmented. No idea if it works, but it compiles.

--- Musik.Core/src/MusikPlaylist.cpp
+++ Musik.Core/src/MusikPlaylist.cpp
@@ -1,5 +1,6 @@
 #include "stdafx.h"
 #include "../include/MusikPlaylist.h"
+#include "../include/MusikLibrary.h"
 
 //-----------------------------------------------------//
 //--- CMusik Song: a single song ID					---//
@@ -17,7 +18,9 @@ CMusikSong::~CMusikSong()
 
 CStdString CMusikSong::GetField( int field )
 {
-	return _T( "" );
+	CStdString ret;
+	m_MusikLibrary->GetFieldFromID( m_ID, field, ret );
+	return ret;
 }
 
 void CMusikSong::SetField( int field, CStdString value )
@@ -28,9 +31,6 @@ void CMusikSong::SetField( int field, CStdString value )
 
 CMusikSongInfo::CMusikSongInfo()
 {
-	m_Filename = m_Title = m_Artist = m_Album = m_Year =
-	m_Genre = m_LastPlayed = m_TimeAdded = m_Notes = _T( "" );
-
 	m_ID = m_Rating = m_Bitrate = m_Duration = m_Dirty =
 	m_Filesize = m_TimesPlayed = m_TrackNum = m_Format =
 	m_VBR = -1;
@@ -39,4 +39,12 @@ CMusikSongInfo::CMusikSongInfo()
 CMusikSongInfo::~CMusikSongInfo()
 {
 
+}
+
+//-----------------------------------------------------//
+//--- CMusikPlaylist: a playlist object				---//
+//-----------------------------------------------------//
+CStdString CMusikPlaylist::GetField( int index, int field )
+{
+	return m_Songs.at( index ).GetField( field );
 }
