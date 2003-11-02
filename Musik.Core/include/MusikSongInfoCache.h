4e96d1f1b1d96736563525774f5fcc06d4e48e67
avatar5d <avatar5d>
2003-11-02 01:23:36 +0000

    Updates

--- Musik.Core/include/MusikSongInfoCache.h
+++ Musik.Core/include/MusikSongInfoCache.h
@@ -20,6 +20,57 @@ public:
 		if ( from == m_From && to == m_To )
 			return;
 
+		// we only have new items at the bottom of 
+		// the list
+		if ( from == m_From && to != m_To )
+		{
+			// we have more items to ADD to the
+			// bottom of the list
+			if ( to > m_To )
+			{
+				
+			}
+
+			// we have items to REMOVE from the
+			// bottom of the list
+			else if ( to < m_To )
+			{
+
+			}
+
+			m_To = to;
+			return;
+		}
+
+		// we have new items in the top of the
+		// list, but not at the bottom
+		else if ( to == m_To && from != m_From )
+		{
+			// we have more items to ADD to the 
+			// top
+			if ( from > m_From )
+			{
+
+			}
+
+			// have have items to REMOVE from 
+			// the top
+			else if ( from < m_From )
+			{
+				
+			}
+
+			m_From = from;
+			return;
+		}
+
+		// we have a completely new set of data, so
+		else
+		{
+
+
+		}
+
 		m_From = from; 
 		m_To = to;
 	}
