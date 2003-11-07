4a27b3ae7ec5bec4a1a8dbfc564f229cf17f1861
avatar5d <avatar5d>
2003-11-07 05:41:48 +0000

    No idea if this stuff works, but added initialization code for fmod for the core.

--- Musik.Core/include/MusikPlayer.h
+++ Musik.Core/include/MusikPlayer.h
@@ -25,9 +25,14 @@ enum
 
 enum
 {
+	// initialization values
 	MUSIK_PLAYER_INIT_START = 0,
 	MUSIK_PLAYER_INIT_STOP,
-	MUSIK_PLAYER_INIT_RESTART
+	MUSIK_PLAYER_INIT_RESTART,
+
+	// return values
+	MUSIK_PLAYER_INIT_SUCCESS,
+	MUSIK_PLAYER_INIT_ERROR
 };
 
 ///////////////////////////////////////////////////
@@ -45,7 +50,7 @@ public:
 	CMusikPlayer( CMusikFunctor* functor );
 	~CMusikPlayer();
 
-	int  InitSound( int mode = MUSIK_PLAYER_INIT_START );
+	int  InitSound( int device, int driver, int rate, int channels, int mode = MUSIK_PLAYER_INIT_START );
 
 	void SetPlaylist( CMusikPlaylist* playlist );
 	
@@ -59,22 +64,30 @@ public:
 	bool IsPaused();
 
 private:
+	// start and stop sound. InitSound() will
+	// relay to these helper functions
+	void StopSound();
+	int  StartSound( int device, int driver, int rate, int channels );
+
+	// status flags
 	bool m_IsPlaying;
 	bool m_IsPaused;
 
+	// a pointer to a functor that will be
+	// used to post a next song event
 	CMusikFunctor* m_Functor;
 
+	// active streams and channels
 	CMusikStreamPtrArray* m_ActiveStreams;
 	CIntArray* m_ActiveChannels;
 
+	// main thread and mutex
 	ACE_Thread_Mutex main_mutex;
 	ACE_Thread* main_thread;
 
+	// pointer to library and playlist
 	CMusikLibrary* m_Library;
 	CMusikPlaylist* m_Playlist;
-
-	void* m_NewSong;
-	void CallNewSongPtr();
 };
 
 ///////////////////////////////////////////////////
--- Musik.Core/src/MusikPlayer.cpp
+++ Musik.Core/src/MusikPlayer.cpp
@@ -23,6 +23,91 @@ CMusikPlayer::~CMusikPlayer()
 
 ///////////////////////////////////////////////////
 
+int CMusikPlayer::InitSound( int device, int driver, int rate, int channels, int mode )
+{
+	if ( mode == MUSIK_PLAYER_INIT_RESTART || mode == MUSIK_PLAYER_INIT_STOP )
+		StopSound();
+
+	if ( mode == MUSIK_PLAYER_INIT_START || mode == MUSIK_PLAYER_INIT_RESTART )
+		return StartSound( device, driver, rate, channels );
+
+	return MUSIK_PLAYER_INIT_SUCCESS;
+}
+
+///////////////////////////////////////////////////
+
+int CMusikPlayer::StartSound( int device, int driver, int rate, int channels )
+{
+	int nOSDriverID = -1;
+
+	#ifdef WIN32
+		switch( driver )
+		{
+		case 0:
+			nOSDriverID = FSOUND_OUTPUT_DSOUND;
+			break;
+		case 1:
+			nOSDriverID = FSOUND_OUTPUT_WINMM;
+			break;
+		case 2:
+			nOSDriverID = FSOUND_OUTPUT_ASIO;
+			break;
+		}
+	#else
+		switch( driver )
+		{
+		case 0:
+			nOSDriverID = FSOUND_OUTPUT_DSOUND;
+			break;
+		case 1:
+			nOSDriverID = FSOUND_OUTPUT_WINMM;
+			break;
+		case 2:
+			nOSDriverID = FSOUND_OUTPUT_ASIO;
+			break;
+		}
+	#endif
+
+	if ( nOSDriverID == -1 )
+		return MUSIK_PLAYER_INIT_ERROR;
+
+	// initialize the driver that will be used
+	// for the device.
+	//
+	// windows supports the following:
+	//   - direct sound
+	//   - windows waveform
+	//   - asio
+	//
+	// linux supports the following:
+	//   - open sound system (OSS)
+	//   - enlightenment sound daemon (ESD)
+	//   - advanced linux sound architecture (ALSA)
+	if( !FSOUND_SetOutput( nOSDriverID ) )
+		return MUSIK_PLAYER_INIT_ERROR;
+
+	// initialize the device itself.
+	if ( !FSOUND_SetDriver( driver ) )
+		return MUSIK_PLAYER_INIT_ERROR;
+
+	// now initialize the whole system
+	FSOUND_SetBufferSize( 100 );
+	if ( !FSOUND_Init( rate, channels, 0 ) )
+		return MUSIK_PLAYER_INIT_ERROR;
+
+	// if we get here we're home free
+	return MUSIK_PLAYER_INIT_SUCCESS;
+}
+
+///////////////////////////////////////////////////
+
+void CMusikPlayer::StopSound()
+{
+	FSOUND_Close();
+}
+
+///////////////////////////////////////////////////
+
 bool CMusikPlayer::Play( int index )
 {
 
