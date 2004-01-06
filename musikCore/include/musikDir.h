a4afb7178cb82f8d51f2a6e608e991d4524235de
avatar5d <avatar5d>
2004-01-06 03:32:38 +0000

    Comments

--- musikCore/include/musikDir.h
+++ musikCore/include/musikDir.h
@@ -1,4 +1,43 @@
 ///////////////////////////////////////////////////
+// 
+// Info:
+//
+//   musik is a cross platform, open source
+//   multimedia library. More information at:
+//
+//     http://musik.berlios.de
+//     http://musik.sourceforge.net
+//
+// Copyright and Credits:
+//
+//   Copyright      : Casey Langen, 2003
+//   Casey Langen   : Lead Developer, Project Manager. E-Mail: casey@bak.rr.com
+//   Dustin Carter  : Developer, pain in the ass
+//   Simon Windmill : Developer, the most helpful person thats immediately available ;)
+//
+// License:
+//
+//   See license.txt included with this distribution
+//
+///////////////////////////////////////////////////
+//
+// Class(s): 
+//
+//   CmusikDir
+//
+// Filename(s): 
+//
+//   musikDir.h, musikDir.cpp
+//
+// Information:
+//
+//   Cross platform (win32 / linux) directory traverser
+// 
+// Usage: 
+//
+//   Construct then call Run()
+//
+///////////////////////////////////////////////////
 
 #ifndef C_MUSIK_DIR_H
 #define C_MUSIK_DIR_H
