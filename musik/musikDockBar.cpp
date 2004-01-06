e46770030a64ab7ec0b71ab291b723a26bd2b55c
avatar5d <avatar5d>
2004-01-06 03:51:21 +0000

    A few more comments.

--- musik/musikDockBar.cpp
+++ musik/musikDockBar.cpp
@@ -1,4 +1,47 @@
 ///////////////////////////////////////////////////
+// 
+// Info:
+//
+//   musik is a cross platform, open source
+//   multimedia library. More information at:
+//
+//     http://musik.berlios.de
+//     http://musik.sourceforge.net
+//
+// Copyright and Credits:
+//
+//   Copyright      : Casey Langen, 2003
+//   Casey Langen   : Lead Developer, Project Manager. E-Mail: casey@bak.rr.com
+//   Dustin Carter  : Developer, pain in the ass
+//   Simon Windmill : Developer, the most helpful person thats immediately available ;)
+//
+// License:
+//
+//   See license.txt included with this distribution
+//
+///////////////////////////////////////////////////
+//
+// Class(es): 
+//   CmusikDockBar
+//
+// Filename(s): 
+//
+//   musikDockBar.h, musikDockBar.cpp
+//
+// Information:
+//
+//   A CSizingControlBar specifically adpated for the
+//   musikCube UI
+// 
+// Usage: 
+//
+//   Same as a CSizingControlBar... see URL below
+//
+// Some code adapted from:
+//
+//   Cristi Posea: http://www.datamekanix.com/sizecbar/
+//
+///////////////////////////////////////////////////
 
 #include "stdafx.h"
 
