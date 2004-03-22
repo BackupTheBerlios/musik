dbb3082ddbb0a582596e996c65d0b671e7b05f74
avatar5d <avatar5d>
2004-03-22 10:28:54 +0000

    Updates the default icon

--- musik/Resource.h
+++ musik/Resource.h
@@ -11,7 +11,6 @@
 #define IDD_DIALOG2                     131
 #define IDD_DYNPLAYLISTEDITOR           131
 #define IDI_MUSIK_16                    133
-#define IDI_MUSIK_32                    134
 #define IDI_ICON1                       135
 #define IDI_MUSIK_48                    135
 #define IDD_SAVESTDPLAYLIST             137
@@ -42,6 +41,8 @@
 #define IDD_TAG_PROPERTIES              161
 #define IDD_PROPPAGE_TUNAGE             162
 #define IDD_WINAMP_VIS                  163
+#define IDI_ICON2                       166
+#define IDI_MUSIK_32                    166
 #define IDC_CHECK_STARTUPSCAN           1007
 #define IDC_CHECK_STARTUPSCAN2          1008
 #define IDC_CHECK_PL_STRIPES            1009
@@ -298,7 +299,7 @@
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE        165
+#define _APS_NEXT_RESOURCE_VALUE        167
 #define _APS_NEXT_COMMAND_VALUE         32943
 #define _APS_NEXT_CONTROL_VALUE         1071
 #define _APS_NEXT_SYMED_VALUE           104
